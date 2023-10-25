625e021b04a0ba0630e3ae8b43dd35ccdc9c7207
Joshua Jacobs <joshua.jacobs126@gmail.com>
2023-10-25 18:40:50 -0500

    DI framework set up across projects

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            Assembly assembly = typeof(DependencyInjection).Assembly;
+
+            return services;
+        }
+    }
+}
