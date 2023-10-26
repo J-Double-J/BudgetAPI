e2df97310039099b9d682b014ba66ec4495efca0
Joshua Jacobs <joshua.jacobs126@gmail.com>
2023-10-25 19:34:49 -0500

    Domain has result library added

---
+++ Domain/Abstract/ValidationError.cs
@@ -0,0 +1,55 @@
+﻿using Domain.CustomFluentValidation;
+using Newtonsoft.Json;
+
+namespace Domain.Abstract
+{
+    /// <summary>
+    /// Class for Validation Errors.
+    /// </summary>
+    public class ValidationError : Error
+    {
+
+        /// <summary>
+        /// Validation Error for constructor.
+        /// </summary>
+        /// <param name="messageHeader">String to display in message before error. Note that a new line is inserted at the end.</param>
+        /// <param name="errorMessage">Error message for validation.</param>
+        public ValidationError(ErrorCode errorCode, string errorMessage)
+            : base(errorCode, errorMessage)
+        {
+            DetermineIfValidationLevelIsInternal(errorCode);
+        }
+
+        public ValidationError(ErrorCode errorCode, string errorMessage, bool isInternal)
+            : base(errorCode, errorMessage, isInternal)
+        {
+        }
+
+        [JsonIgnore]
+        public BudgetValidatorType ValidationErrorLevel { get; init; }
+
+        /// <summary>
+        /// Determines if the error code type is an internal error. This can be used to mask errors when returned in the API.
+        /// </summary>
+        /// <param name="validationLevel">What level the error occurred at.</param>
+        /// <exception cref="InvalidOperationException">Throws if a <see cref="BudgetValidatorType.Mixed"/> is passed in. Errors
+        /// must </exception>
+        private void DetermineIfValidationLevelIsInternal(ErrorCode errorCode)
+        {
+            BudgetValidatorType validatorLevel = ValidationErrorCodeFactory.ValidationLevelFromCode(errorCode);
+
+            // Likely subject to change at some point as errors become more specific. These enums might be extracted to classes to have behavior.
+            switch (validatorLevel)
+            {
+                case BudgetValidatorType.None:        IsInternalError = false; break;
+                case BudgetValidatorType.Entity:      IsInternalError = false; break;
+                case BudgetValidatorType.Database:    IsInternalError = true; break;
+                case BudgetValidatorType.ExternalCommunication:   IsInternalError = false; break;
+                case BudgetValidatorType.IO:                      IsInternalError = true; break;
+                case BudgetValidatorType.Configuration:           IsInternalError = true; break;
+                case BudgetValidatorType.Mixed:       throw new InvalidOperationException("Validation Errors must not be mixed and must be assigned.");
+                case BudgetValidatorType.Unknown:     IsInternalError = true; break; // We assume we did not account for an internal problem, or code was improperly formmatted.
+            }
+        }
+    }
+}
---
+++ Domain/BudgetValidationErrorCodeHelper.cs
@@ -0,0 +1,113 @@
+﻿using Domain.Abstract;
+using Domain.Primitives;
+using System.Text;
+
+namespace Domain.CustomFluentValidation
+{
+    /// <summary>
+    /// This enum clarifies at what level of the application the validator is concerned with.
+    /// This also means that all validation rules internally should be as relevant to the level as possible.
+    /// </summary>
+    public enum BudgetValidatorType
+    {
+        None = 0,
+
+        Entity = 10,
+
+        Database = 20,
+
+        ExternalCommunication = 30,
+
+        IO = 40,
+
+        Configuration = 50,
+
+        Request = 60,
+
+        // Mixed means that a validator could have various levels and the error code will be
+        // set at the validation result level.
+        Mixed = 98,
+
+        Unknown = 99
+    }
+
+    public static class ValidationErrorCodeFactory
+    {
+        public const string ValidationCodeRoot = "ValidationFailure";
+        public const string EntityErrorCode = $"{ValidationCodeRoot}.Entity";
+        public const string DatabaseErrorCode = $"{ValidationCodeRoot}.Database";
+        public const string ExternalCommunicationErrorCode = $"{ValidationCodeRoot}.ExternalCommunication";
+        public const string IOErrorCode = $"{ValidationCodeRoot}.IO";
+        public const string ConfigurationErrorCode = $"{ValidationCodeRoot}.Configuration";
+        public const string RequestErrorCode = $"{ValidationCodeRoot}.Request";
+        public const string UnknownErrorCode = $"{ValidationCodeRoot}.Unknown";
+
+        private static readonly KeyValuePair<BudgetValidatorType, string>[] budgetValidatorLevelToString = new[]
+        {
+            new KeyValuePair<BudgetValidatorType, string>(BudgetValidatorType.Entity, EntityErrorCode),
+            new KeyValuePair<BudgetValidatorType, string>(BudgetValidatorType.Database, DatabaseErrorCode),
+            new KeyValuePair<BudgetValidatorType, string>(BudgetValidatorType.ExternalCommunication, ExternalCommunicationErrorCode),
+            new KeyValuePair<BudgetValidatorType, string>(BudgetValidatorType.IO, IOErrorCode),
+            new KeyValuePair<BudgetValidatorType, string>(BudgetValidatorType.Request, RequestErrorCode),
+            new KeyValuePair<BudgetValidatorType, string>(BudgetValidatorType.Unknown, UnknownErrorCode),
+        };
+
+        public static readonly BidirectionalDictionary<BudgetValidatorType, string> ValidatorAndStringBidirectionalDict = new(budgetValidatorLevelToString);
+
+        /// <summary>
+        /// Constructs a valid Budget Validation Error Code
+        /// </summary>
+        /// <param name="validationType">Type of validation that failed</param>
+        /// <param name="specifiedErrorCode">End of error code that specifies what exactly went wrong.</param>
+        /// <returns>A constructed error code following Domain rules.</returns>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="specifiedErrorCode"/> is null.</exception>
+        public static ErrorCode ConstructErrorCode(BudgetValidatorType validationType, string specifiedErrorCode)
+        {
+            if (string.IsNullOrEmpty(specifiedErrorCode))
+            {
+                throw new ArgumentNullException(nameof(specifiedErrorCode), "Specified Error Code cannot be null or empty." +
+                    "If you must construct an error code try calling `PrefixErrorCodeIfNoPrefixAttached()` to track BudgetValidatorType");
+            }
+
+            return PrefixErrorCodeIfNoPrefixAttached(validationType, specifiedErrorCode);
+        }
+
+        /// <summary>
+        /// Attaches the Validation error prefix if there is no prefix attached. If <paramref name="errorCode"/> is null, its set to "NonSpecified"
+        /// </summary>
+        /// <param name="validationType">LType of validation that failed.</param>
+        /// <param name="errorCode">Error code to prefix</param>
+        /// <returns>A constructed error code following Domain rules.</returns>
+        public static ErrorCode PrefixErrorCodeIfNoPrefixAttached(BudgetValidatorType validationType, string errorCode)
+        {
+            if (!string.IsNullOrEmpty(errorCode) && errorCode.StartsWith(ValidationCodeRoot))
+            {
+                return ErrorCode.ConstructFromStringRepresentation(errorCode);
+            }
+
+            if (string.IsNullOrEmpty(errorCode))
+            {
+                errorCode = "NonSpecific";
+            }
+
+            if (ValidatorAndStringBidirectionalDict.TryGetByFirstKey(validationType, out string standardCode))
+            {
+                return ErrorCode.ConstructFromCombinedTypeDomain(standardCode, errorCode);
+            }
+
+            return ErrorCode.ConstructFromCombinedTypeDomain(UnknownErrorCode, errorCode);
+        }
+
+        public static BudgetValidatorType ValidationLevelFromCode(ErrorCode code)
+        {
+            string prefix = $"{code.ErrorType}.{code.ErrorDomain}";
+
+            if (ValidatorAndStringBidirectionalDict.TryGetBySecondKey(prefix, out var validationLevel))
+            {
+                return validationLevel;
+            }
+
+            return BudgetValidatorType.Unknown;
+        }
+    }
+}
